bb2f8e5493d157d4fc0556c6b1a82249fcb81aec
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-01-19 23:15:32 +0400

    first commit

---
+++ day 1/day1.py
@@ -0,0 +1,7 @@
+name = "gamarjoba me var nikoloz "
+surname = "tchitadze da var "
+age = "15 wlis "
+height = "170 cm simagle "
+information = "da GOA shemayvara CHAD damfudznebelma :nika keshelavam)))"
+fraud = " da tu ramea mosyidva ar aris😊"
+print( name + surname + age + height + information + fraud)
