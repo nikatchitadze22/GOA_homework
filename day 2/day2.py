19e806521c345d3c465d1367cd529ee0440d5015
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-02-12 20:44:06 +0400

    commit

---
+++ day 2/day2.py
@@ -0,0 +1,30 @@
+GOA = ("me GOA - shi gavicani mxolod da mxolod mizanmimartuli xalxi romlebic statistikad ar iqcevian, me gavicani: ")
+people = ("  1.gigaxuco_27197") 
+people_1 = ("  2.s.s8431") 
+people_2 = ("  3.g1osha") 
+people_3 = ("  4.dadianidatoo_80010") 
+people_4 = ("  5.sabachikhl1821 ")
+people_5 = ("  6.iicheesy")
+people_6 = ("  7.lukito231")
+people_7 = ("  8.sandrozabakhidze")
+people_8 = ("  9.data_tezelashvili")
+people_9 = (" 10.razma_25")
+people_10 = ("  11.tabuka915_26643")
+people_11 = ("  12. .cyanz")
+people_12 = ("  13.nika_keshelava__goa")
+
+my_opinion = ("               pirvelma gakvetilma chemze dzalian didi gavlena iqonia, davuaxlovdi bavshvebs romlebsac arc ki vicnobdi")
+my_opinion_2 = (" da es dzalian momewona, gavecani bavshvebis azrebs, movusmine dzalian bevr saintereso kitxvebsa da ufro saintereso pasuxebi")
+my_opinion_3 = (" da madloba nika maswavlebels, radgan man chamoayaliba aseti gunduri da megobruli garemoeba sadac yvelas sheudzlia gamotkvas tavisi azri da matze miigos pasuxi")
+my_opinion_4 = (" da rogorc batonma lukam tqva:: GOA - shi sheudzlebeli araferia")
+
+group_1 = people_6 + people_7 + people_8 + people_9 + people_10 + people_11 + people_12
+group_2 = GOA + people + people_1 + people_2 + people_3 + people_4 + people_5 
+group_3 = my_opinion + my_opinion_2 + my_opinion_3 + my_opinion_4
+
+print(group_2 + group_1 + group_3 )
+
+
+
+
+
