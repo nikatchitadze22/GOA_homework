7024db3485745217c085c86efa04f2ddb690f6a9
Nikoloz Tchitadze <157101398+nikatchitadze22@users.noreply.github.com>
2024-02-26 18:28:33 +0400

    Delete day 6/day 6 classwork directory

--- day 6/day 6 classwork/day6.py
+++
@@ -1,5 +0,0 @@
-name = input("your name is: ")
-lastname = input("your lastname is: ")
-age = int(input("your age is: "))
-
-print(f"hi as i understood you are {name} {lastname} and you are {age} years old")
