0842fa66aaf72754f779cd425d81fb64b65e08f3
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-10 21:37:16 +0400

    commit

--- day 10/day10homework/day10homework(1).py
+++ day 10/day10homework/day10homework(1).py
@@ -1,2 +1,8 @@
-programming_language = ['ruby', 'java', 'python', 'php', 'c*']
-print(programming_language[4])
+# programming_language = ['ruby', 'java', 'python', 'php', 'c*']
+# print(programming_language[4])
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for i in num:
+    print(num[i] * 2)
