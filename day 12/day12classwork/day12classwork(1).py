6afd3e37572f7b1543528368c88892ca2620893b
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-14 20:51:29 +0400

    commit

---
+++ day 12/day12classwork/day12classwork(1).py
@@ -0,0 +1,5 @@
+name = 'sadguri' 
+list = ['filmi', 'seriali', 'multfilmi', 'komedia']
+print(name[-5:-2])
+print(list[-3:-1])
+
---
+++ day 12/day12classwork/day12classwork(2).py
@@ -0,0 +1,2 @@
+name_lastname = 'nikoloz tchitadze'
+print(name_lastname[0:7])
