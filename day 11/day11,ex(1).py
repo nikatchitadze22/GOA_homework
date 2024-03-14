52035810dcadc06c8f9d5421dec0f706fe302b3b
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-14 20:53:06 +0400

    commit

--- day 11/day11,ex(1).py
+++ day 11/day11,ex(1).py
@@ -1,7 +1,5 @@
 list = [110, 12, 42, -61, 20, -215, 300, 192, 10, 72, 391]
-# print(list[0] + list[1] + list[2] + list[3] + list[4] + list[5] + list[6] + list[7] + list[8] + list[9] + list[10])
-# print(list[0] - list[1] - list[2] - list[3] - list[4] - list[5] - list[6] - list[7] - list[8] - list[9] - list[10])
-# print(list[0] / list[1] / list[2] / list[3] / list[4] / list[5] / list[6] / list[7] / list[8] / list[9] / list[10])
-# print(list[0] * list[1] * list[2] * list[3] * list[4] * list[5] * list[6] * list[7] * list[8] * list[9] * list[10])
-for i in list:
-    print(list[i] * 2)
+print(list[0] + list[1] + list[2] + list[3] + list[4] + list[5] + list[6] + list[7] + list[8] + list[9] + list[10])
+print(list[0] - list[1] - list[2] - list[3] - list[4] - list[5] - list[6] - list[7] - list[8] - list[9] - list[10])
+print(list[0] / list[1] / list[2] / list[3] / list[4] / list[5] / list[6] / list[7] / list[8] / list[9] / list[10])
+print(list[0] * list[1] * list[2] * list[3] * list[4] * list[5] * list[6] * list[7] * list[8] * list[9] * list[10])
