5ed98ee2492aa3a3b73974c88e4f00afc4bced30
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-15 22:55:16 +0400

    commit

--- day 12/homework/day12homework(5).py
+++ day 12/homework/day12homework(5).py
@@ -1,9 +1,5 @@
-# მომხმარებელს შემოატანინეთ აკადემიის სახელი. თუ ის "G"-თი იწყება, 
-# დაუპრინტეთ რომ გოა არის საუკეთესო არჩევანი. სხვა შემთხვევაში დაუპრინტეთ,
-# რომ არასწორი გადაწყვეტილება მიიღო.
-
 academy = input('what academy is best: ')
-if academy[0] == 'G' and 'g':
+if academy[0] == 'G':
     print('GOA is the best')
 else:
     print('wrong choice that will have dire consequences')
