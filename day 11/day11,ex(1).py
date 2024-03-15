101ae7b25a621c61c29fb2c017a5cb2882c29908
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-15 15:08:34 +0400

    commit

--- day 11/day11,ex(1).py
+++ day 11/day11,ex(1).py
@@ -1,5 +1,13 @@
 list = [110, 12, 42, -61, 20, -215, 300, 192, 10, 72, 391]
-print(list[0] + list[1] + list[2] + list[3] + list[4] + list[5] + list[6] + list[7] + list[8] + list[9] + list[10])
-print(list[0] - list[1] - list[2] - list[3] - list[4] - list[5] - list[6] - list[7] - list[8] - list[9] - list[10])
-print(list[0] / list[1] / list[2] / list[3] / list[4] / list[5] / list[6] / list[7] / list[8] / list[9] / list[10])
-print(list[0] * list[1] * list[2] * list[3] * list[4] * list[5] * list[6] * list[7] * list[8] * list[9] * list[10])
+# print(list[0] + list[1] + list[2] + list[3] + list[4] + list[5] + list[6] + list[7] + list[8] + list[9] + list[10])
+# print(list[0] - list[1] - list[2] - list[3] - list[4] - list[5] - list[6] - list[7] - list[8] - list[9] - list[10])
+# print(list[0] / list[1] / list[2] / list[3] / list[4] / list[5] / list[6] / list[7] / list[8] / list[9] / list[10])
+# print(list[0] * list[1] * list[2] * list[3] * list[4] * list[5] * list[6] * list[7] * list[8] * list[9] * list[10])
+
+
+
+res = []
+
+for i in list:
+    res.append(i)
+    res = sum(res)
