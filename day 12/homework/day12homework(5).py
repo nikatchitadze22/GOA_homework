78093c68256527b0ddee95d6b296e7c845e80d76
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-15 23:41:52 +0400

    commit

--- day 12/homework/day12homework(5).py
+++ day 12/homework/day12homework(5).py
@@ -1,5 +1,5 @@
 academy = input('what academy is best: ')
-if academy[0] == 'G':
+if academy[0] == 'G' or academy[0] == 'g':
     print('GOA is the best')
 else:
     print('wrong choice that will have dire consequences')
