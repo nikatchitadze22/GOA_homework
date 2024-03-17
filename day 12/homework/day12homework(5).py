75e7510d4054d61b647a26091565160cc5b8c2f9
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-17 23:05:42 +0400

    commit

--- day 12/homework/day12homework(5).py
+++ day 12/homework/day12homework(5).py
@@ -1,6 +1,8 @@
-academy = input('what academy is best: ')
-if academy[0] == 'G' or academy[0] == 'g':
-    print('GOA is the best')
-else:
-    print('wrong choice that will have dire consequences')
+# academy = input('what academy is best: ')
+# if academy[0] == 'G' or academy[0] == 'g':
+#     print('GOA is the best')
+# else:
+#     print('wrong choice that will have dire consequences')
 
+name = "gabrieli"
+print(name[:5])
