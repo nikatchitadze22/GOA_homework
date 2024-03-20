dc46824fdd5a0563c65dbd4e943d48382ebbe3be
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-20 14:06:15 +0400

    commit

--- day 13/day13homework.py
+++ day 13/day13homework.py
@@ -1,3 +1,7 @@
-list = ['apple', 'banana', 'pear', 'melon', 'watermelon']
-for i in list:
-    print(i)
+# list = ['apple', 'banana', 'pear', 'melon', 'watermelon']
+# for i in list:
+#     print(i)
+
+
+for x in range(100):
+    print(str(x)+ "levani samsonidze")
