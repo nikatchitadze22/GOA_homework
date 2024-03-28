6034d13e68eece8364ee3d7b46f588abac32cd2d
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-03-28 17:48:09 +0400

    commit

--- day15/classwork/day15(codewars)/day15classwork(codewars)5.py
+++ day15/classwork/day15(codewars)/day15classwork(codewars)5.py
@@ -1,1 +1,12 @@
+def min_funk(arr) :
 
+    start_num = arr[0]
+
+
+    for i in arr:
+        if start_num < i:
+            start_num = i
+
+    return start_num
+
+print(min_funk([1,5,2,8,4,6,11,64,23,7,9,]))
