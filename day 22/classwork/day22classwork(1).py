f81e3bddab5fc02d22ad20884a633993c488192e
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-04-22 11:34:35 +0400

    commit

--- day 22/classwork/day22classwork(1).py
+++ day 22/classwork/day22classwork(1).py
@@ -2,9 +2,9 @@
 # თქვენ უნდა დაითვალოთ სიაში ამ ელემენტის რაოდენობა. 
 # წინა დავალების მსგავსაც, აქაც for ციკლი გამოიყენეთ
 def manual_count(nums, item):
-    count = []
+    count = 0
     for i in nums:
         if i == item:
-            count.append(i)
+            count = count + 1
     print(count)
 manual_count([1,2,3,4,5], 2)
--- day 22/homework/day22homework.py
+++ day 22/homework/day22homework.py
@@ -2,6 +2,6 @@ def manual_pop(list, index):
     new_list = []
     for i in list:
         if i != list[index]:
-        new_list.append(i)
+            new_list.append(i)
     print(new_list)
 manual_pop([1,2,3,4,5,6], 4)
