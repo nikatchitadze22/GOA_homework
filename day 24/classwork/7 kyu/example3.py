05f60867d0b536b8507a768dd0ec23c33f4952fb
nikatchitadze <tchitadzenikoloz@gmail.com>
2024-05-09 21:01:05 +0400

    commit

--- day 24/classwork/7 kyu/example3.py
+++ day 24/classwork/7 kyu/example3.py
@@ -1,6 +1,5 @@
 def remove_smallest(numbers):
     a = numbers[:]
     if a:
-        a.pop(min(a))
+        a.remove(min(a))
     return a
-print([1,2,3,4,5,6,7])
--- day 24/homework/day24homework(3).py
+++ day 24/homework/day24homework(3).py
@@ -1,19 +1,9 @@
-# user_input = input("enter the word: ")
+user_input = input("enter the word: ")
 
-# new = ""
-# for i in user_input:
-#     if i.isupper():
-#         new += i.lower()
-#     elif i.islower():
-#         new += i.upper()
-# print(new)
-
-
-# print("happy birthday".capitalize())
-
-
-# item = "headphone"
-# print(item.upper())
-# print(item)
-
-print("robot".find("o"))
+new = ""
+for i in user_input:
+    if i.isupper():
+        new += i.lower()
+    elif i.islower():
+        new += i.upper()
+print(new)
